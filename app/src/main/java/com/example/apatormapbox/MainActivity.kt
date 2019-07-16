47523cab78cd362f8aa69fdbb96e18d0105593fb
MrGaussa <stanislawmol97@gmail.com>
2019-07-16 12:25:29 +0200

    Działający fragment ustawień

--- app/src/main/java/com/example/apatormapbox/MainActivity.kt
+++ app/src/main/java/com/example/apatormapbox/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.apatormapbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newFragment = SettingsFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_conteiner, newFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
     }
 }
