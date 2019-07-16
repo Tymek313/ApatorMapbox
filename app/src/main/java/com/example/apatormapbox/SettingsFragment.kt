99190d327c6ddf36ddbd4485df41a8662609dc30
MrGaussa <stanislawmol97@gmail.com>
2019-07-16 11:38:46 +0200

    xml/preferences + SettingsFragment

--- app/src/main/java/com/example/apatormapbox/SettingsFragment.kt
+++ app/src/main/java/com/example/apatormapbox/SettingsFragment.kt
@@ -3,25 +3,15 @@ package com.example.apatormapbox
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+
 /**
  * A simple [Fragment] subclass.
  *
  */
-class SettingsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_settings, container, false)
-            
-        return view
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preference)
     }
-
-
 }
