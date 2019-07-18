ecda069846c1c578ff0017d7c3be61ec00112d35
Piotr Tymowski <piotr.tymowski@apator.com>
2019-07-18 14:12:08 +0200

    Dodane entity szczegółów stacji oraz konwertery list

--- app/src/main/java/com/example/apatormapbox/repositiories/SolarRepository.kt
+++ app/src/main/java/com/example/apatormapbox/repositiories/SolarRepository.kt
@@ -3,19 +3,26 @@ package com.example.apatormapbox.repositiories
 import com.example.apatormapbox.database.dao.StationDao
 import com.example.apatormapbox.interfaces.SolarApi
 import com.example.apatormapbox.mappers.JsonToBasicStationEntity
+import com.example.apatormapbox.mappers.JsonToStationEntity
 import com.example.apatormapbox.models.dbentities.StationBasicEntity
-import com.example.apatormapbox.models.stationdetails.StationDetails
+import com.example.apatormapbox.models.dbentities.StationDetailsEntity
 import retrofit2.Response
 
 class SolarRepository(private val api: SolarApi, private val stationDao: StationDao) : BaseRepository() {
 
-    suspend fun getStationDetails(stationId: Int): StationDetails? {
+    suspend fun getStationDetailsFromApi(stationId: String): StationDetailsEntity? {
         return safeApiCall(
             call = {
-                api.getStationDetails(stationId).await()
+                val data = api.getStationDetails(stationId).await().body()
+                val mappedDetails = JsonToStationEntity.map(data!!)
+                Response.success(mappedDetails)
             },
             errorMessage = "Error fetching stationDetails"
         )
+    }
+
+    suspend fun getStationDetailsFromDb(stationId: String): StationDetailsEntity? {
+        return stationDao.getStationDetails(stationId)
     }
 
     suspend fun getStationsFromDb(): List<StationBasicEntity>? {
