ebef440a5f29df4a5e61a381464d50b4c2149926
Piotr Tymowski <piotr.tymowski@apator.com>
2019-07-18 14:12:08 +0200

    Dodane entity szczegółów stacji oraz konwertery list

--- app/src/main/java/com/example/apatormapbox/models/dbentities/StationDetailsEntity.kt
+++ app/src/main/java/com/example/apatormapbox/models/dbentities/StationDetailsEntity.kt
@@ -1,33 +1,61 @@
 package com.example.apatormapbox.models.dbentities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "station_details")
 data class StationDetailsEntity(
-    var id: String? = null,
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    var id: String,
+    @ColumnInfo(name = "lat")
     var lat: Double? = null,
+    @ColumnInfo(name = "lon")
     var lon: Double?,
+    @ColumnInfo(name = "elev")
     var elev: Double?,
+    @ColumnInfo(name = "tz")
     var tz: Int?,
+    @ColumnInfo(name = "location")
     var location: String?,
+    @ColumnInfo(name = "city")
     var city: String?,
+    @ColumnInfo(name = "state")
     var state: String?,
+    @ColumnInfo(name = "solar_resource_file")
     var solarResourceFile: String?,
+    @ColumnInfo(name = "distance")
     var distance: Int?,
+    @ColumnInfo(name = "poa_monthly")
     var poaMonthly: List<Double?>?,
+    @ColumnInfo(name = "dc_monthly")
     var dcMonthly: List<Double?>?,
+    @ColumnInfo(name = "ac_monthly")
     var acMonthly: List<Double?>?,
+    @ColumnInfo(name = "ac_annual")
     var acAnnual: Double?,
+    @ColumnInfo(name = "solrad_monthly")
     var solradMonthly: List<Double?>?,
+    @ColumnInfo(name = "solrad_annual")
     var solradAnnual: Double?,
+    @ColumnInfo(name = "capacity_factor")
     var capacityFactor: Double?,
+    @ColumnInfo(name = "ac")
     var ac: List<Int>?,
+    @ColumnInfo(name = "poa")
     var poa: List<Int>?,
+    @ColumnInfo(name = "dn")
     var dn: List<Int>?,
+    @ColumnInfo(name = "dc")
     var dc: List<Int>?,
+    @ColumnInfo(name = "df")
     var df: List<Int>?,
+    @ColumnInfo(name = "tamb")
     var tamb: List<Int>?,
+    @ColumnInfo(name = "tcell")
     var tcell: List<Int>?,
+    @ColumnInfo(name = "wspd")
     var wspd: List<Int>?
 )
